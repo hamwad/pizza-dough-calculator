dbca148c743922a8de401efd3696120253066e79
hamishwaddy <hamishwaddy@gmail.com>
2025-11-20 11:12:43 +1300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
+  base: "/pizza-dough-calculator/",
   plugins: [
     vue(),
     tailwindcss(),
